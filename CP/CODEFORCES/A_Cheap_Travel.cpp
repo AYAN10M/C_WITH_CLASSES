2cff16921680d1a11e426cb66fd7bc1953d623a5
AYAN10M <gopalchandrahaldar1997@gmail.com>
2024-08-13 09:28:51 +0530

    Cheap Travel Done

--- CP/CODEFORCES/A_Cheap_Travel.cpp
+++ CP/CODEFORCES/A_Cheap_Travel.cpp
@@ -3,25 +3,27 @@ using namespace std;
 
 int main()
 {
-    /*
-        n: the number of rides Ann has planned
-        m: the number of rides covered by the m ride ticket
-        a: the price of a one ride ticket
-        b: the price of an m ride ticket.
-    */
     int n, m, a, b;
     cin >> n >> m >> a >> b;
     int sum = 0;
 
-    while (n--)
+    while (n > 0)
     {
-        if (b > a * m && m <= n)
+        if (b < a * m && n >= m)
         {
             sum += b;
+            n -= m;
         }
+        else if (n < m && b < a * n)
+        {
+            sum += b;
+            n = 0;
+        }
+        
         else
         {
             sum += a;
+            n --;
         }
     }
 
