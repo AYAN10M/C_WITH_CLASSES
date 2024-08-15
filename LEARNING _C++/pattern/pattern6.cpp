1ecac60f5c5909d7b1155219525ed6f27a9b8651
AYAN10M <gopalchandrahaldar1997@gmail.com>
2024-08-15 18:15:17 +0530

    Minor Changes added

--- LEARNING _C++/pattern/pattern6.cpp
+++ LEARNING _C++/pattern/pattern6.cpp
@@ -12,7 +12,16 @@ using namespace std;
 int main()
 {
     int row;
-    cout 
-    
+    cout << "Enter the no of row:\t";
+    cin >> row;
+
+    for (int i = row; i > 0; i--)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << j << "\t";
+        }
+        cout << endl;
+    }
     return 0;
 }
---
+++ LEARNING _C++/pattern/pattern7.cpp
@@ -0,0 +1,33 @@
+/*
+            *
+           ***
+          *****
+         *******
+        *********
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int row;
+    cout << "Enter the number of row:\t";
+    cin >> row;
+    for (int  i = 0; i < row; i++)
+    {
+        for (int k = 0; i < ; i++)
+        {
+            /* code */
+        }
+        
+        for (int j = 0; j < row; j++)
+        {
+            /* code */
+        }
+        
+    }
+
+    
+    return 0;
+}
