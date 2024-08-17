f0bca2d0510a1f0dc4ed5015755b7832fb3cf80c
AYAN10M <gopalchandrahaldar1997@gmail.com>
2024-08-17 21:46:50 +0530

    pattern no 7 done

--- LEARNING _C++/pattern/pattern7.cpp
+++ LEARNING _C++/pattern/pattern7.cpp
@@ -1,10 +1,10 @@
 /*
-            *
-           ***
-          *****
-         *******
-        *********
-*/
+        *
+       ***
+      *****
+     *******
+    *********
+ */
 
 #include <bits/stdc++.h>
 using namespace std;
@@ -14,20 +14,26 @@ int main()
     int row;
     cout << "Enter the number of row:\t";
     cin >> row;
-    for (int  i = 0; i < row; i++)
-    {
-        for (int k = 0; i < ; i++)
-        {
-            /* code */
-        }
-        
-        for (int j = 0; j < row; j++)
-        {
-            /* code */
-        }
-        
-    }
 
+    for (int i = 1; i <= row; i++)
+    {
+        // loop for spacing
+        for (int k = row - i; k > 0 ; k--)
+        {
+            cout << " ";
+        }
+        
+        // loop for printing "*"
+        for (int j = 1; j <= 2 * i - 1; j++)
+        {
+            cout << "*";
+        }
+        
+        
+        cout << endl;
+           
+    }
     
+
     return 0;
 }
