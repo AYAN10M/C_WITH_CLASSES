8b1f54e159f21bacaaa8431cbf37d335b2219b63
AYAN10M <gopalchandrahaldar1997@gmail.com>
2024-08-20 18:52:16 +0530

    Pattern 10 added

--- LEARNING _C++/pattern/pattern10.cpp
+++ LEARNING _C++/pattern/pattern10.cpp
@@ -3,13 +3,11 @@ using namespace std;
 
 int main()
 {
-    int row;
+    int row, i;
     cout << "Enter the no of row:\t";
     cin >> row;
 
-    int half = row / 2;
-
-    for (int i = 1; i <= row; i++)
+    for (i = 1; i <= row/2; i++)
     {
         for (int j = 1; j <= i; j++)
         {
@@ -17,5 +15,16 @@ int main()
         }
         cout << endl;
     }
+
+    for (int k = i; k > 0; k--)
+    {
+        for (int l = 0; l < k; l++)
+        {
+            cout << "*";
+        }
+        i-- ; 
+        cout << endl;
+    }
+
     return 0;
 }
